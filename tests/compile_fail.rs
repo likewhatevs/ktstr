4cc24de7b25dc4d97038d9196cd4e5b90ba2095c
Pat Somaru <patso@likewhatevs.io>
2026-05-27 12:33:38 -0700

    ignore compile_fail trybuild driver
    Signed-off-by: Pat Somaru <patso@likewhatevs.io>

--- tests/compile_fail.rs
+++ tests/compile_fail.rs
@@ -1,5 +1,5 @@
 #[test]
-#[ignore = "many trybuild fixtures, each spawning cargo build; run explicitly with --run-ignored=all"]
+#[ignore]
 fn compile_fail() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile_fail/*.rs");
